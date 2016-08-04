729dca454d7e0197f05b491c95fb5cb2c377211a
Carsten <info@cvantum.de>
2016-08-04 18:05:24 +0200

    Connect fixed to database

--- modules/core.js
+++ modules/core.js
@@ -7,6 +7,7 @@ var assert = require('assert');
 
 class MongoCommands {
 	constructor() {
+		console.log('Staring Mongo-Commants');
 	}
 	//Insert into Collection "server"
 	mongoUpdateServer(db,callback) {
@@ -30,11 +31,11 @@ class MongoCommands {
 }
 
 
-exports.Commands = class Commands {
+exports.Commands = class Commands extends MongoCommands{
 	constructor(config) {
 		super();
 		this.config = config;
-		this.url = 'mongodb://'+config.mongodb_user+':'+config.mongodb_pass+'@localhost:20729/?authMechanism=DEFAULT&authSource='+config.mongodb;
+		this.url = 'mongodb://'+config.mongodb_user+':'+config.mongodb_pass+'@localhost:20729/'+config.mongodb+'?authMechanism=DEFAULT&authSource='+config.mongodb;
 		this.testConnect();
 	}
 	//Test MongoDB-Connection
@@ -48,16 +49,32 @@ exports.Commands = class Commands {
 
 	//Opening the MongoDB-Connection
 	incoming(msg) {
-		MongoClient.connect(url, function(err, db) {
+		console.log(this.url);
+		MongoClient.connect(this.url, function(err, db) {
 			assert.equal(null, err);
 			console.log("Connected correctly to server");
-			super.mongoFindServer(db, function() {
-				db.close();
-			});
+			var collection = db.collection('server');
+			collection.find({}).toArray(function(err, docs) {
+				assert.equal(err, null);
+				console.log(docs);
+				});
+			//this.mongoFindServer(db, function() {
+			//	db.close();
+			//});
 		});
 	}
 
 	response(answer) {
 		console.log(answer);
+	}
+	//Find Discord-Server by ID
+	mongoFindServer(db,callback) {
+		//var collection = db.collection('server');
+		//collection.find({}).toArray(function(err, docs) {
+		//	assert.equal(err, null);
+		//	console.log("Found the following records");
+		//	console.log(docs)
+		//	callback(docs);
+		//});
 	}
 }
